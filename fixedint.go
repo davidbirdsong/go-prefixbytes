6a2d9d2b602f4f307e189ff63be57bc0d9cde3b6
david birdsong <david@imgix.com>
2017-01-02 00:59:51 +0000

    first pass

---
+++ fixedint.go
@@ -0,0 +1,141 @@
+package msgio
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"sync"
+
+	msgio "github.com/jbenet/go-msgio"
+	mpool "github.com/jbenet/go-msgio/mpool"
+)
+
+var PrefixBytes int = 2
+
+func readInt(b []byte) uint32 {
+	// equivalnt of return int32(binary.LittleEndian.Uint32(b))
+	switch cap(b) {
+	case 8:
+		return uint32(b[0])
+	case 16:
+		return uint32(binary.LittleEndian.Uint16(b))
+	case 32:
+		return uint32(binary.LittleEndian.Uint32(b))
+	case 64:
+		return uint32(binary.LittleEndian.Uint64(b))
+	}
+	return 0
+
+}
+
+// fixedintReader is the underlying type that implements the Reader interface.
+type fixedintReader struct {
+	R    io.Reader
+	lbuf []byte
+	next int
+	pool *mpool.Pool
+	lock sync.Locker
+}
+
+// NewVarintReader wraps an io.Reader with a fixedint msgio framed reader.
+// The msgio.Reader will read whole messages at a time (using the length).
+// Varints read according to https://golang.org/pkg/encoding/binary/#ReadUfixedint
+// Assumes an equivalent writer on the other side.
+func NewFixedintReader(r io.Reader, prefbytes int) msgio.ReadCloser {
+	return NewFixedintReaderWithPool(
+		bufio.NewReader(r),
+		prefbytes,
+		&mpool.ByteSlicePool,
+	)
+}
+
+// NewVarintReaderWithPool wraps an io.Reader with a fixedint msgio framed reader.
+// The msgio.Reader will read whole messages at a time (using the length).
+// Varints read according to https://golang.org/pkg/encoding/binary/#ReadUfixedint
+// Assumes an equivalent writer on the other side. It uses a given mpool.Pool
+func NewFixedintReaderWithPool(r io.Reader, prefbytes int, p *mpool.Pool) msgio.ReadCloser {
+	if p == nil {
+		panic("nil pool")
+	}
+	return &fixedintReader{
+		R:    r,
+		lbuf: make([]byte, prefbytes),
+		next: -1,
+		pool: p,
+		lock: new(sync.Mutex),
+	}
+}
+
+// NextMsgLen reads the length of the next msg into s.lbuf, and returns it.
+// WARNING: like Read, NextMsgLen is destructive. It reads from the internal
+// reader.
+func (s *fixedintReader) NextMsgLen() (int, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	return s.nextMsgLen()
+}
+
+func (s *fixedintReader) nextMsgLen() (int, error) {
+	if s.next == -1 {
+		if n, err := s.R.Read(s.lbuf); err != nil {
+			return 0, err
+		} else if n != cap(s.lbuf) {
+			return 0, fmt.Errorf("prefix buffer resized")
+		}
+
+		length := readInt(s.lbuf)
+		s.next = int(length)
+	}
+	return s.next, nil
+}
+
+func (s *fixedintReader) Read(msg []byte) (int, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	length, err := s.nextMsgLen()
+	if err != nil {
+		return 0, err
+	}
+
+	if length > len(msg) {
+		return 0, io.ErrShortBuffer
+	}
+	_, err = io.ReadFull(s.R, msg[:length])
+	s.next = -1 // signal we've consumed this msg
+	return length, err
+}
+
+func (s *fixedintReader) ReadMsg() ([]byte, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	length, err := s.nextMsgLen()
+	if err != nil {
+		return nil, err
+	}
+
+	msgb := s.pool.Get(uint32(length))
+	if msgb == nil {
+		return nil, io.ErrShortBuffer
+	}
+	msg := msgb.([]byte)[:length]
+	_, err = io.ReadFull(s.R, msg)
+	s.next = -1 // signal we've consumed this msg
+	return msg, err
+}
+
+func (s *fixedintReader) ReleaseMsg(msg []byte) {
+	s.pool.Put(uint32(cap(msg)), msg)
+}
+
+func (s *fixedintReader) Close() error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if c, ok := s.R.(io.Closer); ok {
+		return c.Close()
+	}
+	return nil
+}
---
+++ fixedint_test.go
@@ -0,0 +1,58 @@
+package msgio
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"testing"
+)
+
+var msgs = [][]byte{
+	[]byte("foo"),
+	[]byte("badkfja akfjkadjf  akdfja ka kadjfa"),
+	[]byte(`{"FileSize":22897614,"xxxxxxMIMEType":"image/gif","FileType":"GIF","PixelHeight":480,"FileExtension":"gif","Count":130,"PixelWidth":852}`),
+}
+
+func TestLengthPref(t *testing.T) {
+
+	for _, b := range msgs {
+		pref := make([]byte, 16)
+		binary.LittleEndian.PutUint16(pref, uint16(len(b)))
+		buf := bytes.NewBuffer(pref)
+		buf.Write(b)
+
+		fr := NewFixedintReader(buf, 16)
+		if i, err := fr.NextMsgLen(); err != nil {
+			t.Fatal(err)
+		} else if i != len(b) {
+			t.Fatal(fmt.Errorf("NextMsgLen returned [%d] for buffer size: [%d]", i, len(b)))
+		}
+
+	}
+
+}
+
+func TestMsgStream(t *testing.T) {
+
+	var buf = &bytes.Buffer{}
+	for _, b := range msgs {
+		pref := make([]byte, 16)
+		binary.LittleEndian.PutUint16(pref, uint16(len(b)))
+		buf.Write(pref)
+		buf.Write(b)
+	}
+	fr := NewFixedintReader(buf, 16)
+	for _, b := range msgs {
+		if bstream, err := fr.ReadMsg(); err != nil {
+			t.Fatal(err)
+		} else if !bytes.Equal(bstream, b) {
+			t.Error(
+				fmt.Errorf(
+					"bytes not equal, source:[%s], from stream:[%s]",
+					b,
+					bstream,
+				),
+			)
+		}
+	}
+}
